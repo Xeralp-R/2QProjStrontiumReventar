c2229a717e31845d009f1fe884c7c3baf36d1c43
Xer-Alp <80310657+Xeralp-R@users.noreply.github.com>
2022-02-07 20:47:44 +0800

    Got toc working on mobile

--- scripts/subpage.js
+++ scripts/subpage.js
@@ -1,3 +1,5 @@
+let body_var = document.querySelector("main");
+
 /* to get the 1 button */
 let menu_button = document.getElementById("open_menu");
 let menu_dropdown = document.getElementById("desktop_nav");
@@ -12,6 +14,30 @@ menu_button.addEventListener("click", () => {
         menu_dropdown.style.animationName = "hover-out";
     else menu_dropdown.style.animationName = "hover-in";
 
-    menu_dropdown.style.animationDuration = "0.5s"; /* how fast/slow will the animation will be executed */
-    menu_dropdown.style.animationFillMode = "forwards"; /*what will happen after the animation */
+    menu_dropdown.style.animationDuration = "0.5s";
+    /* how fast/slow will the animation will be executed */
+    menu_dropdown.style.animationFillMode = "forwards";
+    /*what will happen after the animation */
+});
+
+let toc_button = document.getElementById("open_toc");
+let toc_content = document.getElementsByTagName("aside")[0];
+
+window.addEventListener("scroll", () => {
+    toc_content.style.top = `${window.pageYOffset + 50}px`;
+});
+
+toc_button.addEventListener("click", () => {
+    console.log("clicked toc!");
+    console.log(window.pageYOffset);
+
+    /* text which @keyframes to assign */
+    if (toc_content.style.animationName == "fly-in-aside")
+        toc_content.style.animationName = "fly-out-aside";
+    else toc_content.style.animationName = "fly-in-aside";
+
+    toc_content.style.animationDuration = "0.5s";
+    /* how fast/slow will the animation will be executed */
+    toc_content.style.animationFillMode = "forwards";
+    /*what will happen after the animation */
 });
